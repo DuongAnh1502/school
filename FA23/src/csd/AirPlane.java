5fe2421d46ed8b691e580a6ac67e736fa646aa40
duonganh203 <rankill52@gmail.com>
2023-10-13 13:52:52 +0700

    finish

--- FA23/src/csd/AirPlane.java
+++ FA23/src/csd/AirPlane.java
@@ -2,12 +2,17 @@ package csd;
 import java.io.*;
 import java.util.*;
 public class AirPlane {
+    //Chuyen input danh sach canh sang danh sach ke
     static HashMap<String,ArrayList<Vertex>> map = new HashMap<>();
     static Scanner sc = new Scanner(System.in);
+    static HashMap<String,String> parrent = new HashMap<>();
+    static HashMap<String,Boolean> visited = new HashMap<>();
+    static ArrayList<String> ls = new ArrayList<>();
+    //Lay vung mien cua tinh,thanh pho
     static String getRegion(String v) {
         final String[] north = {"Mien Bac","hanoi", "tuyenquang", "laocai", "yenbai", "sonla", "hoabinh", "dienbien", "langson", "quangninh", "bacgiang", "phuthuoc", "vinhphuc", "bacninh", "haiduong", "thaibinh", "namdinh", "hanam", "thainguyen", "hungyen", "vinhphuc"};
         final String[] cen = {"Mien Trung","thanhhoa", "nghean", "hatinh", "quangbinh", "quangtri", "thuathienhue", "danang", "quangnam", "quangnai", "binhdinh", "phuyen", "khanhhoa", "ninhthuan", "binhthuan", "kontum", "gialai", "daklak", "daknong"};
-        final String[] south = {"Mien Nam","saigon","cantho", "dongnai", "binhduong", "tphochiminh", "binhphuoc", "tayninh", "angiang", "kiengiang", "camau", "baclieu", "soctrang", "travinh", "vinhlong", "bentre", "longan", "tiengiang", "dongthap"};
+        final String[] south = {"Mien Nam","saigon","cantho", "dongnai", "binhduong", "hochiminh", "binhphuoc", "tayninh", "angiang", "kiengiang", "camau", "baclieu", "soctrang", "travinh", "vinhlong", "bentre", "longan", "tiengiang", "dongthap","phuquoc"};
         String[][] regions = {north, cen, south};
         for (String[] reg : regions) {
             for (int i = 1;i<reg.length;i++) {
@@ -18,9 +23,40 @@ public class AirPlane {
         }
         return "Invalid";
     }
-    static void dijkstra(String s,String r) {
+    static void dfs(String p) {
+        visited.put(p, true);
+        ls.add(p);
+        for(Vertex v:map.get(p)) {
+            if(!visited.get(v.getV())) {
+                dfs(v.getV());
+            }
+        }
+    }
+    //Find cutting point
+    static void findCuttingPoint() {
+        ArrayList<String> rs = new ArrayList<>();
+        for(String v:map.keySet()) {
+            visited.replaceAll((key,value)->false);
+            visited.put(v, true);
+            ls.clear();
+            dfs("danang");
+            if(ls.size() != map.size()-1) {
+                rs.add(v);
+            }
+        }
+        System.out.println("Cutting point : ");
+        for(String v:rs) {
+            System.out.print(v+ " ");
+        }
+        System.out.println("");
+    }
+    //Thuat toan tim duong di ngan nhat
+    static void dijkstra(String s,String t,String r) {
+        //Tao priority queue theo khoang cach cua 2 dinh
         PriorityQueue<Vertex> PQ = new PriorityQueue<>(Comparator.comparingInt(Vertex::getValue));
         HashMap<String,Integer> d = new HashMap<>();
+        ArrayList<String> rs = new ArrayList<>();
+        //Dat khoang cach cua cac dinh thanh gia tri lon nhat
         map.forEach((key,value) -> d.put(key,Integer.MAX_VALUE));
         d.put(s,0);
         PQ.add(new Vertex(s,0));
@@ -34,23 +70,37 @@ public class AirPlane {
                 int value = e.getValue();
                 if(d.get(name) > d.get(v) + value) {
                     d.put(name,d.get(v)+value);
+                    parrent.put(name,v);
                     PQ.add(new Vertex(name,d.get(name)));
                 }
             }
         }
+        dfs("danang");
         d.forEach((key,value) -> {
             if(!key.equalsIgnoreCase(s)) System.out.println(s+ " to "+key+" : "+value);
         });
+        while(t!=s) {
+            rs.add(t);
+            t = parrent.get(t);
+        }
+        rs.add(s);
+        Collections.reverse(rs);
+        for(String n:rs) {
+            System.out.print(n+" ");
+        }
+        System.out.println("");
     }
     static void run() {
         File currentDir = new File("");
-        try(BufferedReader br = new BufferedReader(new FileReader(currentDir.getAbsolutePath()+"/FA23/src/data/flight.txt"))) {
+        try(BufferedReader br = new BufferedReader(new FileReader(currentDir.getAbsolutePath()+"/src/data/flight.txt"))) {
             String line;
             while((line = br.readLine())!=null) {
                 String[] inp = line.split(" ");
                 String v1 = inp[0];
                 String v2 = inp[1];
                 int value = Integer.parseInt(inp[2]);
+                visited.put(v1, false);
+                visited.put(v2,false);
                 if(map.get(v1) != null) {
                     map.get(v1).add(new Vertex(v2,value));
                 } else {
@@ -67,10 +117,11 @@ public class AirPlane {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        dijkstra("danang","Mien Nam");
+        dijkstra("danang","condao","Mien Nam");
     }
     public static void main(String[] args) {
         run();
+        findCuttingPoint();
     }
 }
 class Vertex {
